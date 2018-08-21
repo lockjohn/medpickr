af8bffb887e6daea6d88356d3420d8c3c2098304
lockjohn <lockhartjohn527@gmail.com>
2018-08-20 17:42:44 -0700

    insert canvas into index.html

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
     canvasEl.height = 650;
 
     const ctx = canvasEl.getContext("2d");
-    const game = new Game();
-    new GameView(game, ctx).start();
+    ctx.fillStyle = "purple";
+    ctx.fillRect(0, 0, 1000, 650);
+
+    ctx.beginPath();
+    ctx.arc(100, 100, 20, 0, 2 * Math.PI, true);
+    // ctx.strokeStyle = "green";
+    // ctx.lineWidth = 5;
+    // ctx.stroke();
+    ctx.fillStyle = "blue";
+    ctx.fill();
 });
